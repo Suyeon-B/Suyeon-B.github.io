7efc0c1d6ad89e56e0c8ebdce05bee9c1aa2ff23
Suyeon-B <tndus4243@naver.com>
2021-12-29 23:40:09 +0900

    about me 수정

--- about.js
+++ about.js
@@ -1,4 +1,4 @@
-const content = "안녕하세요! \n 외국어와 개발 공부를 즐기는 개발자, 박수연입니다. :)";
+const content = "안녕하세요! \n 외국어와 개발 공부를 즐기는 박수연입니다. :)";
 const text = document.querySelector(".text");
 let i = 0;
 
@@ -9,5 +9,9 @@ function typing(){
         text.textContent = "";
         i = 0;
     }
+    else{
+        //끝나면 반복종료 
+        clearInterval(tyInt); 
+    }
 }
-setInterval(typing, 200)
+setInterval(typing, 150)
